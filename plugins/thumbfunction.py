6c13ab305469559ef2943d4b84ce8868fb54e838
Khurban05 <119966317+Khurban05@users.noreply.github.com>
2022-12-28 00:44:46 +0500

    Update thumbfunction.py

--- plugins/thumbfunction.py
+++ plugins/thumbfunction.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from helper.database import find, delthumb, addthumb
 
-@Client.on_message(filters.private & filters.command(['hozirgipechatrasm']))
+@Client.on_message(filters.private & filters.command(['thumb']))
 async def viewthumb(client,message):
 		print(message.chat.id)
 		thumb = find(int(message.chat.id))[0]
@@ -11,7 +11,7 @@ async def viewthumb(client,message):
 			await message.reply_text("**Sizda hech qanday pechat rasm yo'q!!!\n\nPechat rasm qo'yish uchun shunchaki rasm yuboring.**")
 	
 	
-@Client.on_message(filters.private & filters.command(['rasmniochirish']))
+@Client.on_message(filters.private & filters.command(['delthumb']))
 async def removethumb(client,message):
 	delthumb(int(message.chat.id))
 	await message.reply_text("**Maxsus pechat rasm muvaffaqiyatli oʻchirildi✅**")
@@ -20,4 +20,4 @@ async def removethumb(client,message):
 async def addthumbs(client,message):
 	file_id = str(message.photo.file_id)
 	addthumb(message.chat.id , file_id)
-	await message.reply_text("**Maxsus pechat rasm muvaffaqiyatli saqlandi✅**")
+	await message.reply_text("**✅Pechat uchun rasm saqlandi.**")
